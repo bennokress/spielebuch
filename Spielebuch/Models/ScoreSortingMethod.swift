29ba012b2ef205a024b447d64a2428d99f265f6d
BK | Mobile Apps <account@bennokress.de>
2019-05-18 22:44:25 +0200

    [#1 ScoreSortingMethod] Files added

---
+++ Spielebuch/Models/ScoreSortingMethod.swift
@@ -0,0 +1,13 @@
+//
+//  ScoreSortingMethod.swift
+//  Spielebuch
+//
+//  Created by Benno Kress on 18.05.19.
+//  Copyright © 2019 Benno Kress. All rights reserved.
+//
+
+import Foundation
+
+enum ScoreSortingMethod {
+    
+}
